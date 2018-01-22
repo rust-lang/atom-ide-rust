371df32484b74a3bef58c66986b4db7bfe59576a
Alex Butler <alexheretic@gmail.com>
2018-01-22 10:53:41 +0000

    Add eslint env: es6 & browser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
   extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: 6,
-  },
   env: {
+    es6: true,
     node: true,
+    browser: true,
   },
   rules: {
     semi: ["error", "never"],
-    'no-console': "off",
+    "no-console": "off",
   },
   globals: {
-    Promise: true,
     atom: true,
   },
 };
